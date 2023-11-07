d55e4112892bd239495470754eec2574435ed854
MaiiZuko <maiaramzucco@gmail.com>
2023-11-07 17:44:39 -0300

    update

--- 3349-boas-praticas-java-testes-projeto_inicial/src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
+++ 3349-boas-praticas-java-testes-projeto_inicial/src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
@@ -1,15 +1,68 @@
 package br.com.alura.adopet.api.service;
 
-import org.junit.jupiter.api.Test;
+import java.util.List;
 
+import static org.mockito.BDDMockito.then;
+import static org.mockito.ArgumentMatchers.any;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatcher;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.model.Abrigo;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.model.Tutor;
+import br.com.alura.adopet.api.repository.AdocaoRepository;
+import br.com.alura.adopet.api.repository.PetRepository;
+import br.com.alura.adopet.api.repository.TutorRepository;
+import br.com.alura.adopet.api.validacoes.ValidacaoSolicitacaoAdocao;
+
+@ExtendWith(MockitoExtension.class)
 public class AdocaoServiceTest {
+
+    @InjectMocks //Pro mockito injetar todos esse métodos automaticamente
+    private AdocaoService service;
+
+    @Mock
+    private AdocaoRepository repository;
+
+    @Mock
+    private PetRepository petRepository;
+
+    @Mock
+    private TutorRepository tutorRepository;
+
+    @Mock
+    private EmailService emailService;
+
+    @Mock
+    private List<ValidacaoSolicitacaoAdocao> validacoes;
+
+    @Mock
+    private Pet pet;
+
+    @Mock
+    private Tutor tutor;
+
+    @Mock
+    private Abrigo abrigo;
+
+    @Mock 
+    private SolicitacaoAdocaoDto dto;
 
     @Test
     void deveriaSalvarAdocaoAoSolicitar() {
         //ARRANGE
 
         //ACT
+        service.solicitar(dto);
 
-        //ASSERT 
+        //ASSERT
+        //Mockito verifica se o petRepository teve o método save chamado com qualquer parametro
+        then(petRepository).should().save(any());
     }
 }
